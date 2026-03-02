81d70c7ccaf5180d8baaa1355af8ef38bf76c82c
thealamenthelumiere <kh-syusupova@yandex.ru>
2026-03-02 15:46:28 +0300

    Fix static assets path

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'src', 'public'), {
+    index: 'page.html',
+  });
+  const staticPath = join(__dirname, '..', 'src', 'public');
+  console.log('Serving static from:', staticPath);
+  app.useStaticAssets(staticPath, { index: 'page.html' });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
+  console.log(`Сервер запущен на порту ${port}`);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error('Ошибка при запуске сервера:', err);
+  process.exit(1);
+});
